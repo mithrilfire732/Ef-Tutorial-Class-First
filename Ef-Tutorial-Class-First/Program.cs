2fcf6f197ed98e7ae831e1031355feaa6f30beb0
Alexander Kidd <alexander.t.kidd@gmail.com>
2021-10-07 16:19:28 -0400

    Added orders controller

--- Ef-Tutorial-Class-First/Program.cs
+++ Ef-Tutorial-Class-First/Program.cs
@@ -8,6 +8,11 @@ namespace Ef_Tutorial_Class_First
     {
         async static Task Main(string[] args)
         {
+            var ordCtrl = new OrdersController();
+            var orders = await ordCtrl.GetAll();
+            orders.ForEach(o => Console.WriteLine($"{o.Description} | {o.Customer.Name}"));
+            
+            
             var custCtrl = new CustomersController();
             var customers = await custCtrl.GetAll();
             customers.ForEach(c => Console.WriteLine(c.Name));
---
+++ SalesDbLib/Controllers/OrdersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesDbLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesDbLib.Controllers
+{
+    public class OrdersController
+    {
+        private readonly AppDbContext _context;
+
+        public OrdersController()
+        {
+            _context = new AppDbContext();
+        }
+
+        public async Task<List<Order>> GetAll()
+        {
+            return await _context.Orders.Include(x => x.Customer).ToListAsync();
+        }
+    }
+}
